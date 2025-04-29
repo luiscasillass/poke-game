b00f705a6019f3168f066f1f041bdb93f93438ff
Luis <149264123+luiscasillass@users.noreply.github.com>
2025-04-28 23:01:16 -0600

    Update Actions.jsx

--- src/game/buttons/Actions.jsx
+++ src/game/buttons/Actions.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import '../styles.css';
 
+const Actions = ({ handleAttack }) => {
+  return (
+    <div className="container-action">
+      <div style={{ paddingTop: '40%' }}>
+        <button className="action-btn">B</button>
+      </div>
+      <div>
+        <button className="action-btn" onClick={handleAttack}>A</button>
+      </div>
+    </div>
+  );
+};
+
+export default Actions;
